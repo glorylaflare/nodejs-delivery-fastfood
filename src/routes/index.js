e3c36dca0e48ab0946768c214948311ffb2bc0f4
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-01-04 18:20:40 -0300

    feat: adição de novas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const pizza = require("./pizzaRouter.js");
+const sanduiche = require("./sanduicheRouter.js");
+const bebida = require("./bebidaRouter.js");
+const acompanhamento = require("./acompanhamentoRouter.js");
 
 module.exports = app => {
     app.route("/").get((req, res) => res.status(200).send("O projeto Delivery está funcionando."));
 
     app.use(
         express.json(),
+        pizza,
+        sanduiche,
+        bebida,
+        acompanhamento,
     );
 };
